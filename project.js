2328013b50e14fc289392258ec739a341716073d
AslanHuseynov <64832203+AslanHuseynov@users.noreply.github.com>
2020-07-04 17:45:48 +0400

    Delete project.js

--- project.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	$('#more').on('click', function () {
-  	$('#more-overlay').css('display', 'block');
-  });
-  
-  $('#close-overlay').on('click', function () {
-  	$('#more-overlay').css('display', 'none');
-  });
-});
